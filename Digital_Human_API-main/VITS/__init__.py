5915d1ecf4814ddc6e947a098902bc8c29f74fd3
lihuihan <2216110468@qq.com>
2026-06-18 10:57:27 +0800

    delete:精简文件

--- Digital_Human_API-main/VITS/__init__.py
+++
@@ -1,1 +0,0 @@
-# 使 VITS 目录成为一个包，方便使用绝对导入 VITS.GPT_SoVITS
--- bs/kimi.ps1
+++
@@ -1,4 +0,0 @@
-$env:ANTHROPIC_BASE_URL="https://api.moonshot.cn/anthropic"
-$env:ANTHROPIC_AUTH_TOKEN="
-$env:ANTHROPIC_MODEL="kimi-k2-0711-preview"
-$env:ANTHROPIC_SMALL_FAST_MODEL="kimi-k2-0711-preview"
